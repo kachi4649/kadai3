c41e33b9cdb35df4eb8b72c31099af996e6d2d0d
kachimegatera <s21c1032vh@s.chibakoudai.jp>
2023-02-13 05:44:57 +0900

    gitrm

---
+++ plus.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# SPDX-FileCopyright: 2022 Takuma Kachi  
+# SPDX-License-Identifier: BSD-3-Clause
+
+import sys
+
+#print('sys.argv         : ', sys.argv)
+#print('type(sys.argv)   : ', type(sys.argv))
+#print('len(sys.argv)    : ', len(sys.argv))
+
+#print()
+
+#print('sys.argv[0]      : ', sys.argv[0])
+#print('sys.argv[1]      : ', sys.argv[1])
+#print('sys.argv[2]      : ', sys.argv[2])
+#print('type(sys.argv[0]): ', type(sys.argv[0]))
+#print('type(sys.argv[1]): ', type(sys.argv[1]))
+#print('type(sys.argv[2]): ', type(sys.argv[2]))
+
